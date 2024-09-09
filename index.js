1fdb220d067f44af5f169d1910ef72348932997c
unknown <shadmanxalve@gmail.com>
2024-09-09 14:09:06 +0600

    event

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,27 +10,27 @@ app.use(bodyParser.json());
 
 /* connection establishment */
 app.get("/", (req, res, next) => {
-    try {
-      res.status(200).json({
-        acknowledgement: true,
-        message: "OK",
-        description: "The request is OK",
-      });
-    } catch (err) {
-      next(err);
-    } finally {
-      //console.log(`Route: ${req.url} || Method: ${req.method}`);
-    }
-  });
+  try {
+    res.status(200).json({
+      acknowledgement: true,
+      message: "OK",
+      description: "The request is OK",
+    });
+  } catch (err) {
+    next(err);
+  } finally {
+    //console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+});
 
-app.get('/webhook', (req, res) => {
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
 
   if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      console.log('WEBHOOK VERIFIED');
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("WEBHOOK VERIFIED");
       res.status(200).send(challenge);
     } else {
       res.sendStatus(403);
@@ -38,19 +38,26 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-app.post('/webhook', (req, res) => {
+app.post("/webhook", (req, res) => {
   const event = req.body;
-
-  console.log('Received event:', event);
-
-  // Forward the event to the Node.js app running on port 8081
-  axios.post('https://s4s1vqvr-8081.asse.devtunnels.ms/v1/api/messaging-webhook', event)
-    .then(response => {
-      console.log('Event forwarded successfully:', response.data);
+  console.log("Received event:", event);
+  // Forward the event to the Node.js app running on port 8081 as a JSON string
+  axios
+    .post(
+      "https://s4s1vqvr-8081.asse.devtunnels.ms/v1/api/messaging-webhook",
+      JSON.stringify(event),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      console.log("Event forwarded successfully:", response.data);
       res.sendStatus(200);
     })
-    .catch(error => {
-      console.error('Error forwarding event:', error);
+    .catch((error) => {
+      console.error("Error forwarding event:", error);
       res.sendStatus(500);
     });
 });
